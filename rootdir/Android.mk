455dd1e7e0152db64dea615fa4464a381065d3b5
Dyneteve <dyneteve@pixelexperience.org>
2020-03-18 18:07:58 +0000

    curtana: rootdir: import ramdisk tuning from coral

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -282,3 +282,10 @@ LOCAL_SRC_FILES    := bin/init.insmod.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.power.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.power.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+include $(BUILD_PREBUILT)
